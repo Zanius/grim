892bd4ab6614de477b03ea3fdc43c4dad309567d
Zane Brown <zib1991@gmail.com>
2022-11-20 04:01:08 +0000

    Update mix

--- mix.exs
+++ mix.exs
@@ -1,16 +1,26 @@
 defmodule Grim.MixProject do
   use Mix.Project
 
+  @github "https://github.com/zanius/grim"
+
   def project do
     [
       app: :grim,
+      name: "Grim",
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      homepage: @github,
+      package: package(),
+      description: description()
     ]
+  end
+
+  defp description do
+    "Grim is a pruning library that periodically destroys stale records that are no longer useful."
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -39,6 +49,14 @@ defmodule Grim.MixProject do
       {:postgrex, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Zane Brown"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
     ]
   end
 
